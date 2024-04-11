68acada827ac5e90e9076f9a1c17d8bb41ade30c
SH1RL0CK <50455686+SH1RL0CK@users.noreply.github.com>
2024-04-11 18:19:46 +0200

    Add basic login page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,22 @@
 <svelte:head>
 	<title>Stduy Sphere</title>
 </svelte:head>
-<div class="mx-auto w-2/5">
-	<div class="card bg-base-100 mt-24 shadow-xl">
-		<div class="card-body space-y-2">
-			<h2 class="text-center text-4xl font-extrabold">Hier entsteht Study Sphere</h2>
-			<p class="text-center text-xl">
-				SMOESEL Inc baut gerade das perfekte Campus-Management-System für Ihre Hochschule!
-			</p>
+<div class="mx-2 sm:mx-auto sm:w-4/5 lg:w-2/6">
+	<div class="card mt-24 bg-base-100 shadow-xl">
+		<div class="card-body space-y-2 text-center">
+			<h2 class="text-4xl font-extrabold">Login</h2>
+			<label class="input input-bordered flex items-center gap-2">
+				<i class="fa-solid fa-envelope"></i>
+				<input placeholder="E-Mail" />
+			</label>
+			<label class="input input-bordered flex items-center gap-2">
+				<i class="fa-solid fa-lock"></i>
+				<input placeholder="Passwort" type="password" />
+			</label>
 			<div class="card-actions justify-center">
-				<a class="btn btn-primary" href="https://smoesel.de">Mehr erfahren</a>
+				<a class="btn btn-primary" href="https://smoesel.de">
+					<i class="fa-solid fa-paper-plane"></i> Anmelden
+				</a>
 			</div>
 		</div>
 	</div>
