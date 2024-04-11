072eabefc105e726ec45b2653e582e4c6b226289
SH1RL0CK <50455686+SH1RL0CK@users.noreply.github.com>
2024-04-11 19:08:58 +0200

    Add form validation with svelte superforms

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,25 @@
-import type { PageServerLoad } from './$types';
+import { fail } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+import { message, superValidate } from 'sveltekit-superforms/server';
+import { z } from 'zod';
 
-export const load = (async () => {
-	return {};
-}) satisfies PageServerLoad;
+const loginFormSchema = z.object({
+	email: z.string().email('Bitte geben Sie eine gültige E-Mail-Adresse an'),
+	password: z.string().min(1, 'Geben Sie ein Passwort ein')
+});
+
+export const load = async () => {
+	const form = await superValidate(zod(loginFormSchema));
+	return { form };
+};
+
+export const actions = {
+	default: async ({ request }) => {
+		const form = await superValidate(request, zod(loginFormSchema));
+		console.log(form.data, form.valid);
+		if (!form.valid) {
+			return fail(400, { form });
+		}
+		return message(form, 'Erfolgreich eingeloggt!');
+	}
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,59 @@
+<script>
+	import { superForm } from 'sveltekit-superforms';
+
+	export let data;
+
+	const { form, message, errors, enhance } = superForm(data.form, { resetForm: false });
+</script>
+
 <svelte:head>
 	<title>Stduy Sphere</title>
 </svelte:head>
+
 <div class="mx-2 sm:mx-auto sm:w-4/5 lg:w-2/6">
 	<div class="card mt-24 bg-base-100 shadow-xl">
-		<div class="card-body space-y-2 text-center">
-			<h2 class="text-4xl font-extrabold">Login</h2>
-			<label class="input input-bordered flex items-center gap-2">
-				<i class="fa-solid fa-envelope"></i>
-				<input placeholder="E-Mail" />
-			</label>
-			<label class="input input-bordered flex items-center gap-2">
-				<i class="fa-solid fa-lock"></i>
-				<input placeholder="Passwort" type="password" />
-			</label>
-			<div class="card-actions justify-center">
-				<a class="btn btn-primary" href="https://smoesel.de">
-					<i class="fa-solid fa-paper-plane"></i> Anmelden
-				</a>
+		<form use:enhance method="post">
+			<div class="card-body space-y-2 text-center">
+				<h2 class="text-4xl font-extrabold">Login</h2>
+				<div>
+					<label
+						class="input input-bordered flex items-center gap-2"
+						class:input-error={$errors.email}
+					>
+						<i class="fa-solid fa-envelope"></i>
+						<input name="email" placeholder="E-Mail" bind:value={$form.email} />
+					</label>
+					{#if $errors.email}
+						<div class="label">
+							<span class="label-text-alt text-error">{$errors.email}</span>
+						</div>
+					{/if}
+				</div>
+				<div>
+					<label
+						class="input input-bordered flex items-center gap-2"
+						class:input-error={$errors.password}
+					>
+						<i class="fa-solid fa-lock"></i>
+						<input
+							name="password"
+							placeholder="Passwort"
+							type="password"
+							bind:value={$form.password}
+						/>
+					</label>
+					{#if $errors.password}
+						<div class="label">
+							<span class="label-text-alt text-error">{$errors.password}</span>
+						</div>
+					{/if}
+				</div>
+				<div class="card-actions justify-center">
+					<button class="btn btn-primary">
+						<i class="fa-solid fa-paper-plane"></i> Anmelden
+					</button>
+				</div>
 			</div>
-		</div>
+		</form>
 	</div>
 </div>
