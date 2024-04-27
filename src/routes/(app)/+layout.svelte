1630ec548e7e8c72468048008e08bd8a8081e093
SH1RL0CK <50455686+SH1RL0CK@users.noreply.github.com>
2024-04-27 21:18:33 +0200

    Add navbar with logo and user info

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,21 +1,32 @@
 <script>
-	import { page } from '$app/stores';
 	import logo from '$lib/assets/logo.png';
+
+	export let data;
 </script>
 
-<nav class="navbar bg-primary">
-	<div class="container mx-auto flex justify-between">
-		<a class="btn btn-ghost text-2xl text-white" href="/">
-			<img src={logo} class="h-12 w-12" alt="logo" />Study Sphere</a
-		>
-		{#if $page.url.pathname !== '/'}
-			<a class="btn btn-square btn-ghost text-center text-2xl text-white" href="/logout">
-				<i class="fa-solid fa-right-from-bracket"></i>
+<div class="min-h-screen bg-base-200">
+	<nav class="navbar bg-base-100">
+		<div class="flex w-full justify-between">
+			<a class="btn btn-ghost text-2xl font-bold text-primary" href="/">
+				<img src={logo} class="h-12 w-12" alt="logo" />
+				Study Sphere
 			</a>
-		{/if}
-	</div>
-</nav>
-
-<div class="mx-2 sm:mx-auto sm:w-4/5 lg:w-2/6">
+			<div class="dropdown dropdown-end">
+				<div tabindex="0" role="button" class="avatar placeholder btn btn-circle btn-ghost">
+					<div class="w-10 rounded-full bg-neutral text-neutral-content">
+						<span class="text-lg">{data.initials}</span>
+					</div>
+				</div>
+				<ul
+					class="menu dropdown-content menu-sm z-[1] mt-3 w-36 rounded-box bg-base-100 p-2 shadow"
+				>
+					<li>
+						<a href="/"><i class="fa-solid fa-user-circle"></i> Profil</a>
+					</li>
+					<li><a href="/logout"><i class="fa-solid fa-right-from-bracket"></i> Abmelden</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
 	<slot />
 </div>
