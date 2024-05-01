f56b0dac2c9dced36a4a44b2b93e72f162601739
SH1RL0CK <50455686+SH1RL0CK@users.noreply.github.com>
2024-05-01 20:01:50 +0200

    Add a default module banner

--- src/lib/components/ModulesGrid.svelte
+++ src/lib/components/ModulesGrid.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { type paths } from '$lib/services/api/spec';
 
+	import defaultModuleBanner from '$lib/assets/default_module_banner.webp';
+
 	type Modules = paths['/module/']['get']['responses']['200']['content']['application/json'];
 
 	export let modules: Modules;
@@ -8,17 +10,17 @@
 </script>
 
 <div class="mt-4 grid grid-cols-1 gap-4 lg:grid-cols-2 2xl:grid-cols-4">
-	{#each modules as course}
-		<a class="card bg-base-100 shadow-xl" href="/meine-module/{course.id}">
+	{#each modules as module}
+		<a class="card bg-base-100 shadow-xl" href="/meine-module/{module.id}">
 			<figure>
 				<img
-					src="/file/{course.banner_file_id}"
-					alt={course.name}
+					src={module.banner_file_id ? `/file/${module.banner_file_id}` : defaultModuleBanner}
+					alt={module.name}
 					class="h-40 w-full object-cover"
 				/>
 			</figure>
 			<div class="card-body">
-				<h2 class="text-2xl font-bold">{course.name}</h2>
+				<h2 class="text-2xl font-bold">{module.name}</h2>
 				<p>Matthias Maßmann</p>
 			</div>
 		</a>
